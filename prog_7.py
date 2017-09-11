20a5efb1c8c3cd1e3631b97bde8cad26900ffbb8
ironmanvivekjangir42 <31614933+ironmanvivekjangir42@users.noreply.github.com>
2017-09-11 15:33:07 +0530

    Add files via upload

--- prog_7.py
+++ prog_7.py
@@ -1,7 +1,53 @@
 #!/usr/bin/python3
-from crontab import CronTab
-i=1
-tym=cron.new(command='/usr/bin/echo')
-tym.minute.duration(3,9).every(3)
-print("python",i)
-i=i+1
+#stone paper scisors
+import random
+cc=0
+ps=0
+count=0
+s=["stone","scisor","paper"]
+r=random.randint(0,2)
+c=s[r]
+z=input("press p to play")
+while count<=10:
+	count=count+1
+	if z=="p":
+		i=input("typ stone, scisor ,paper to play")
+		print("player chose",i,",comp chose",c)
+		if i=="stone":
+			if c=="stone":
+				print("its a draw")
+				print("ur score is",ps,",comp score is",cc)
+			elif c=="scisor":
+				print("u won")
+				ps=ps+1
+				print("ur score is",ps,",comp score is",cc)
+			else :
+				print("comp won")
+				cc=cc+1
+				print("ur score is",ps,",comp score is",cc)
+		elif i=="scisor":
+			if c=="stone":
+				print("comp won")
+				cc=cc+1
+				print("ur score is",ps,",comp score is",cc)
+			elif c=="scisor":
+				print("its a draw")
+				print("ur score is",ps,",comp score is",cc)
+			else :
+				print("u won")
+				ps=ps+1
+				print("ur score is",ps,",comp score is",cc)
+		elif i=="paper":
+			if c=="stone":
+				print("u won")
+				ps=ps+1
+				print("ur score is",ps,",comp score is",cc)
+			elif c=="scisor":
+				print("comp won")
+				cc=cc+1
+				print("ur score is",ps,",comp score is",cc)
+			else :
+				print("its a draw")
+				print("ur score is",ps,",comp score is",cc)
+		else:
+			print("wrong spelling,typr again")
