7c9f9dc60fab6eef051bcb87cb946574fd5e4584
ironmanvivekjangir42 <31614933+ironmanvivekjangir42@users.noreply.github.com>
2017-09-28 08:42:51 +0530

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import os
-import time
+import os#importing os libraries
+import time#importing time libraries
 a=0
 b=1
-while a<=10:
-    im=str(b)+".jpg"
+while a<=10:#create a while loop
+    im=str(b)+".jpg"#creating a int to string
     img="/home/pi/camm/imgs/"
     cmd= "fswebcam -F 5 --fps 20 -r \"1200x800\" "+img
-    t=cmd+im
-    os.system(t)
+    t=cmd+im#command
+    os.system(t)#execute the command
     a=a+1
     b=b+1
-    time.sleep(30)
+    time.sleep(30)#add a delay of 30 seconds
