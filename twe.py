269ecfcda870e1fb0c119ceb3acb62560da0991a
ironmanvivekjangir42 <31614933+ironmanvivekjangir42@users.noreply.github.com>
2017-09-28 09:01:11 +0530

    Update twe.py

--- twe.py
+++ twe.py
@@ -1,5 +1,6 @@
 from TwitterAPI import TwitterAPI
 
+#gove required details 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_TOKEN_KEY = ''
@@ -9,8 +10,7 @@ b=1
 while True:
     b=b+1
     api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
-    file = open('/home/pi/cam/imgs/'+str(b)+'.jpg', 'rb')
-    print('done')
+    file = open('/home/pi/cam/imgs/'+str(b)+'.jpg', 'rb')#selecting a file to be uploaded
     data = file.read()
     r = api.request('statuses/update_with_media', {'status':'#pyTweetCMR'}, {'media[]':data})
     print(r.status_code)
