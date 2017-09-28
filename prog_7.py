81aae350ebef66c6569e631a80a02a2a05a7a806
ironmanvivekjangir42 <31614933+ironmanvivekjangir42@users.noreply.github.com>
2017-09-28 08:57:59 +0530

    Update prog_7.py

--- prog_7.py
+++ prog_7.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-#stone paper scisors
 import random
-cc=0
-ps=0
+cc=0#integer to store computers score
+ps=0#int to store players score
 count=0
-s=["stone","scisor","paper"]
-r=random.randint(0,2)
-c=s[r]
+s=["stone","scissor","paper"]#create an array
+r=random.randint(0,2)#create a radom integer 
+c=s[r]#Choosing form the array 
 z=input("press p to play")
 while count<=10:
 	count=count+1
 	if z=="p":
-		i=input("typ stone, scisor ,paper to play")
+		i=input("typ stone, scissor ,paper to play")
 		print("player chose",i,",comp chose",c)
 		if i=="stone":
 			if c=="stone":
 				print("its a draw")
 				print("ur score is",ps,",comp score is",cc)
-			elif c=="scisor":
+			elif c=="scissor":
 				print("u won")
 				ps=ps+1
 				print("ur score is",ps,",comp score is",cc)
@@ -25,12 +24,12 @@ while count<=10:
 				print("comp won")
 				cc=cc+1
 				print("ur score is",ps,",comp score is",cc)
-		elif i=="scisor":
+		elif i=="scissor":
 			if c=="stone":
 				print("comp won")
 				cc=cc+1
 				print("ur score is",ps,",comp score is",cc)
-			elif c=="scisor":
+			elif c=="scissor":
 				print("its a draw")
 				print("ur score is",ps,",comp score is",cc)
 			else :
@@ -42,7 +41,7 @@ while count<=10:
 				print("u won")
 				ps=ps+1
 				print("ur score is",ps,",comp score is",cc)
-			elif c=="scisor":
+			elif c=="scissor":
 				print("comp won")
 				cc=cc+1
 				print("ur score is",ps,",comp score is",cc)
