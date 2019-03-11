4906829badf4b185a6abffa0a40396b2eb623b0d
ironmanvivekjangir42 <31614933+ironmanvivekjangir42@users.noreply.github.com>
2019-03-11 19:58:12 +0530

    Delete vivek_dice.py

--- vivek_dice.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-r=random.randint(1,6)#creates a random integer between 1 and 6
-if r==6:
-	print("hurrayy!!!!!...   u got 6")#print hurray if dice shows 6
-elif r==5:
-    print("good....u got 5")#print this if dice shows 5
-else:
-	print("u got",+r)#prints this if dice shows less then 5
